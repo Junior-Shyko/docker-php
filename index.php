fbf2e31d63e2471c81e8d1e63de09afba615db92
junior.oliveira <junior.oliveira@esp.ce.gov.br>
2022-06-27 18:11:31 -0300

    Add pacote de rotas

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+//https://packagist.org/packages/altorouter/altorouter
 ini_set('display_errors', 1);
 require 'vendor/autoload.php';
 require __DIR__ . '/vendor/altorouter/altorouter/AltoRouter.php';
