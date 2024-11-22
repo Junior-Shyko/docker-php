df5997ac4bc5ff125baa1d92be97420fc7c83411
Junior-Shyko <franciscoanto@gmail.com>
2024-11-21 21:18:25 -0300

    :construction: Adicionado itens com formatado no html

--- estudo-crud/list.php
+++ estudo-crud/list.php
@@ -6,7 +6,36 @@ $db = new Conn();
 
 $personRepo = new Crud($db);
 $persons = $personRepo->getAllUsers();
-dump($persons);
-
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Lista de pessoas</title>
+  <?php require 'assets.php' ?>
+</head>
+
+<body>
+  <div class="container">
+  <ul class="list-group mt-5">
+    <?php foreach ($persons as $key => $person) { ?>
+      <li class="list-group-item">
+        <div class="d-flex justify-content-between">
+          <div class="col-md-9">
+          <?php echo $person['name']; ?>
+          </div>
+          <div class="col-md-3">
+          <button type="button" class="btn btn-primary">Editar</button>
+          <button type="button" class="btn btn-danger">Excluir</button>
+          </div>
+        </div>       
+      </li>
+    <?php } ?>
+    </ul>
+  </div>
+</body>
+
+</html>
